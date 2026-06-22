8826eac8277815b32f6893dd40dcb3caaca0169b
Wake0801 <n22dcvt099@student.ptithcm.edu.vn>
2026-06-22 17:39:55 +0700

    Add score row DTO

---
+++ src/main/java/com/example/thitracnghiem/dto/ScoreRow.java
@@ -0,0 +1,115 @@
+package com.example.thitracnghiem.dto;
+
+import java.time.LocalDate;
+
+public class ScoreRow {
+
+    private String maSv;
+    private String ho;
+    private String ten;
+    private String hoTen;
+    private String maLop;
+    private String tenLop;
+    private String maMh;
+    private String tenMh;
+    private Short lan;
+    private LocalDate ngayThi;
+    private Double diem;
+    private String diemChu;
+
+    public String getMaSv() {
+        return maSv;
+    }
+
+    public void setMaSv(String maSv) {
+        this.maSv = maSv;
+    }
+
+    public String getHo() {
+        return ho;
+    }
+
+    public void setHo(String ho) {
+        this.ho = ho;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getMaLop() {
+        return maLop;
+    }
+
+    public void setMaLop(String maLop) {
+        this.maLop = maLop;
+    }
+
+    public String getTenLop() {
+        return tenLop;
+    }
+
+    public void setTenLop(String tenLop) {
+        this.tenLop = tenLop;
+    }
+
+    public String getMaMh() {
+        return maMh;
+    }
+
+    public void setMaMh(String maMh) {
+        this.maMh = maMh;
+    }
+
+    public String getTenMh() {
+        return tenMh;
+    }
+
+    public void setTenMh(String tenMh) {
+        this.tenMh = tenMh;
+    }
+
+    public Short getLan() {
+        return lan;
+    }
+
+    public void setLan(Short lan) {
+        this.lan = lan;
+    }
+
+    public LocalDate getNgayThi() {
+        return ngayThi;
+    }
+
+    public void setNgayThi(LocalDate ngayThi) {
+        this.ngayThi = ngayThi;
+    }
+
+    public Double getDiem() {
+        return diem;
+    }
+
+    public void setDiem(Double diem) {
+        this.diem = diem;
+    }
+
+    public String getDiemChu() {
+        return diemChu;
+    }
+
+    public void setDiemChu(String diemChu) {
+        this.diemChu = diemChu;
+    }
+}
