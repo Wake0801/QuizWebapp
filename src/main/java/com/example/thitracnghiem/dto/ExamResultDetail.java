0d27d28b6ce0dfcce6bc1a3bc82a6e3559bf5918
Wake0801 <n22dcvt099@student.ptithcm.edu.vn>
2026-06-22 17:39:54 +0700

    Add exam result detail DTO

---
+++ src/main/java/com/example/thitracnghiem/dto/ExamResultDetail.java
@@ -0,0 +1,86 @@
+package com.example.thitracnghiem.dto;
+
+public class ExamResultDetail {
+
+    private int thuTu;
+    private Integer cauHoi;
+    private String noiDung;
+    private String a;
+    private String b;
+    private String c;
+    private String d;
+    private String dapAnChon;
+    private String dapAnDung;
+
+    public int getThuTu() {
+        return thuTu;
+    }
+
+    public void setThuTu(int thuTu) {
+        this.thuTu = thuTu;
+    }
+
+    public Integer getCauHoi() {
+        return cauHoi;
+    }
+
+    public void setCauHoi(Integer cauHoi) {
+        this.cauHoi = cauHoi;
+    }
+
+    public String getNoiDung() {
+        return noiDung;
+    }
+
+    public void setNoiDung(String noiDung) {
+        this.noiDung = noiDung;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+
+    public String getD() {
+        return d;
+    }
+
+    public void setD(String d) {
+        this.d = d;
+    }
+
+    public String getDapAnChon() {
+        return dapAnChon;
+    }
+
+    public void setDapAnChon(String dapAnChon) {
+        this.dapAnChon = dapAnChon;
+    }
+
+    public String getDapAnDung() {
+        return dapAnDung;
+    }
+
+    public void setDapAnDung(String dapAnDung) {
+        this.dapAnDung = dapAnDung;
+    }
+}
